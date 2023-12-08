af0b862200278104d17ae4bc41745d5bc592816c
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-12-08 03:08:24 +0100

    Added signUp & Login form component(Ex-6.3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  // standalone: true,
+  // imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog){}
+  // This is the function that will open the dialog when the signup button is clicked  
+    openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+// Assigning the dialog a width
+    width: '280px'
+  });
+}
+
+  openUserLoginDialog(): void{
+    this.dialog.open(UserLoginFormComponent, {
+      // Assigning the dialog a width 
+      width: '280px'
+    })
+  }
 }
