fbb518936a36789bf3d9941cf46a95195a133f80
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-12-22 02:12:30 +0530

    Updated login component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -34,7 +34,7 @@ export class UserLoginFormComponent implements OnInit{
     this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
       // Logic for a successful user login goes here! (To be implemented)
       console.log(result);
-      localStorage.setItem('username', JSON.stringify(result.user));
+      localStorage.setItem('user', JSON.stringify(result.user));
       localStorage.setItem('token', result.token);
       this.dialogRef.close(); // This will close the modal on success!
       console.log(result);
