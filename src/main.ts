c75cceb13da724145c510ae5b9aef821403eefee
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2024-01-01 13:59:41 +0530

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { appConfig } from './app/app.config';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
